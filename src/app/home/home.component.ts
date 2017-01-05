72ac63c31847e210bb2b9df028e783937f737ff4
Wang, Wei-jye <wei-jye.wang@intel.com>
2017-01-05 21:04:35 +0800

    feat(home): add donut chart
    This commit add a fun donut chart to home page.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+declare let google: any;
 
 @Component({
   selector: 'seed-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent { }
+export class HomeComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    google.charts.load("current", {packages:["corechart"]});
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+      const data = google.visualization.arrayToDataTable([
+        ['Task', 'Hours per Day'],
+        ['Work',     11],
+        ['Eat',      2],
+        ['Commute',  2],
+        ['Watch TV', 2],
+        ['Sleep',    7]
+      ]);
+
+      const options = {
+        title: 'My Daily Activities',
+        pieHole: 0.4,
+      };
+
+      let chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+      chart.draw(data, options);
+    }
+  }
+}
